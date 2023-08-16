d0dfa9324e5e9c1183c87bfe54ca9b2157c164ba
libby <elizabeth.zhang94@gmail.com>
2023-08-15 18:29:49 -0700

    Added iterator to produce complete and incomplete minibatches of indices, with automatic shuffling

---
+++ dtd/utils.py
@@ -0,0 +1,43 @@
+import jax.numpy as jnp
+import jax.random as jr
+
+class ShuffleIndicesIterator():
+    """Custom Iterator that produces minibatches of shuffled indices.
+    
+    Parameters
+        key: PRNGKey
+            Used to shuffle the indices at each iteration.
+        batch_shape: tuple
+            Shape of target array's batch dimensions
+        minibatch_size: int
+            Number of indices per minibatch
+    
+    Outputs
+        batched_indices:   shape (n_batches, minibatch_size, batch_ndims)
+        remaining_indices: shape (n_remaining, batch_ndims)
+    """
+
+    def __init__(self, key, batch_shape, minibatch_size):
+        self.key = key
+
+        # Sequentially enumerated indices, shape (n_samples, batch_ndims)
+        # where n_samples = prod(*batch_shape) and batch_ndims = len(batch_shape)
+        self._indices = jnp.indices(batch_shape).reshape(len(batch_shape), -1).T
+
+        self.minibatch_size = minibatch_size
+        self.n_batches = len(self._indices) // self.minibatch_size
+
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        # Shuffle indices
+        _, self.key = jr.split(self.key)
+        indices = jr.permutation(self.key, self._indices, axis=0)
+
+        # Split indices into complete minibatch sets and an incomplete set
+        batched_indices, remaining_indices \
+                            = jnp.split(indices, (self.n_batches*self.minibatch_size,))
+        batched_indices = batched_indices.reshape(self.n_batches, self.minibatch_size, -1)
+
+        return batched_indices, remaining_indices
