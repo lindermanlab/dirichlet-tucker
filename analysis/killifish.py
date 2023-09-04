3adec6533739852016ed2b8a2a5e41598aeaa16e
libby <elizabeth.zhang94@gmail.com>
2023-09-04 14:59:31 -0700

    use stochastic em algorithm

--- analysis/killifish.py
+++ analysis/killifish.py
@@ -8,6 +8,7 @@ from typing import Union
 import numpy as onp
 import jax.numpy as jnp
 import jax.random as jr
+import optax
 
 from tensorflow_probability.substrates import jax as tfp
 tfd = tfp.distributions
@@ -17,6 +18,17 @@ import wandb
 import matplotlib.pyplot as plt
 
 from dtd.model3d import DirichletTuckerDecomp
+
+PROJECT_NAME = 'kf-dtd'
+DEFAULT_LR_SCHEDULE_FN = (
+    lambda n_minibatches, n_epochs:
+        optax.cosine_decay_schedule(
+            init_value=1.,
+            alpha=0.,
+            decay_steps=n_minibatches*n_epochs,
+            exponent=0.8,
+        )
+)
 
 def _get_subshape(X, axes: tuple=(0,)):
     """Return shape of X along specified axes."""
@@ -44,7 +56,7 @@ def load_data(fpath_list: list[str]):
             ages.append(f['session_ids'])
         
     return {
-        'counts': jnp.concatenate(counts, axis=0),
+        'X': jnp.concatenate(counts, axis=0),
         'batch_axes': (0,1),
         'ages': ages, 
         'names': names
@@ -54,29 +66,54 @@ def make_random_mask(key, shape, train_frac=0.8):
     """Make binary mask to split data into train (1) and test (0) sets."""
     return jr.bernoulli(key, train_frac, shape)
 
-def fit_model(key, X, mask, k1, k2, k3, alpha=1.1, n_iters=5000):
-    d1, d2, d3 = X.shape
-    total_counts = X[0,0].sum() # TODO Make a flexible sum, given batch or normalized axes
+def stochastic_fit_model(key, X, mask, total_counts, k1, k2, k3, alpha=1.1,
+                         lr_schedule_fn=None, minibatch_size=20, n_epochs=100):
+    """Fit 3D DTD model to data using stochastic fit algoirthm.
+
+    (New) parameters
+        lr_schedule_fn: Callable[[int, int], optax.Schedule]
+            Given `n_minibatches` and `n_epochs`, returns a function mapping step
+            counts to learning rate value.
+        minibatch_size: int
+            Number of samples per minibatch.
+        n_epochs: int
+            Number of full passes through the dataset to perform
+    """
+
+    key_init, key_fit = jr.split(key)
+
+    # Set default learning rate schedule function if none provided
+    lr_schedule_fn = lr_schedule_fn if lr_schedule_fn is not None else DEFAULT_LR_SCHEDULE_FN
 
     # Construct a model
     model = DirichletTuckerDecomp(total_counts, k1, k2, k3, alpha)
 
     # Randomly initialize parameters
     print("Initializing model...", end="")
-    init_params = model.sample_params(key, d1, d2, d3)
+    d1, d2, d3 = X.shape
+    init_params = model.sample_params(key_init, d1, d2, d3)
     print("Done.")
 
     # Fit model to data with EM
     print("Fitting model...", end="")
-    params, lps = model.fit(X, mask, init_params, n_iters)
+    params, lps = model.stochasitc_fit(X, mask, init_params, n_epochs,
+                                       lr_schedule_fn, minibatch_size, key_fit)
     print("Done.")
 
+    return params, lps
+
+def evaluate_fit(params, X, mask, total_counts, k1, k2, k3, alpha):
+    """Compute heldout log likelihood and percent deviation from saturated model."""
+
+    # Reonstruct a model 
+    model = DirichletTuckerDecomp(total_counts, k1, k2, k3, alpha)
+    
     # Compute test log likelihood
-    print("Computing test log likelihood...", end="")
+    print("Evaluating fit...", end="")
     test_ll = model.heldout_log_likelihood(X, mask, params)
 
     # Compute test ll under baseline model (empirical average of syllable usage)
-    baseline_probs = jnp.mean(X[mask], axis=0)  # TODO does X[mask] actually still preserve X shape?
+    baseline_probs = jnp.mean(X[mask], axis=0)  # TODO does X[mask] actually still preserve X shape? Shouldn't this sum over batch_axes?
     baseline_probs /= baseline_probs.sum(axis=-1, keepdims=True)
     baseline_test_ll = \
         tfd.Multinomial(total_counts, probs=baseline_probs).log_prob(X[~mask]).sum()
@@ -91,68 +128,79 @@ def fit_model(key, X, mask, k1, k2, k3, alpha=1.1, n_iters=5000):
     pct_dev = (test_ll - baseline_test_ll) / (saturated_test_ll - baseline_test_ll)
     print("Done.")
 
-    return model, params, lps, pct_dev
+    return test_ll, pct_dev
 
 @click.command()
-@click.option('--datadir', type=click.Path(exists=True, file_okay=False),
+@click.argument('datadir', type=click.Path(exists=True, file_okay=False),
                 help='Path to directory of input files.')
-@click.option('--k1', type=int, help='number of factors along dimension 1.')
-@click.option('--k2', type=int, help='number of factors along dimension 2.')
-@click.option('--k3', type=int, help='number of factors along dimension 3.')
-@click.option('--seed', default=0, show_default=True, help='random seed for initialization.')
-@click.option('--alpha', default=1.1, show_default=True, help='concentration of Dirichlet prior.')
-@click.option('--train-frac', default=0.8, show_default=True, help='concentration of Dirichlet prior.')
-@click.option('--n-iters', default=5000, show_default=True, help='number of em iterations to run.')
-@click.option('--project', default=None, show_default=True, help='WandB project name.')
-def run_one(project, datadir, k1, k2, k3, seed, alpha, train_frac=0.8, n_iters=5000):
+@click.option('k1', type=int,)
+@click.option('k2', type=int,)
+@click.option('k3', type=int,)
+@click.option('--alpha', type=float, default=1.1,
+              help='Concentration of Dirichlet prior.')
+@click.option('--seed',type=int, default=0,
+              help='Random seed for initialization.')
+@click.option('--train', 'train_frac', type=float, default=0.8,
+              help='Fraction of .')
+@click.option('--minibatch', 'minibatch_size', type=int, default=20,
+              help='Number of samples per minibatch')
+@click.option('--epoch', 'n_epochs', type=int, default=5000,
+              help='Number of epochs of stochastic EM to run.')
+@click.option('--wandb', 'use_wandb', is_flag=True,
+              help='Log run with WandB')
+def run_one(datadir, k1, k2, k3, seed, alpha, train_frac=0.8,
+            minibatch_size=20, n_epochs=5000, use_wandb=False):
     """Fit data to one set of model parameters."""
-    # k1 = int(k1)
-    k2 = int(k2)
-    k3 = int(k3)
-    seed = int(seed)
-    alpha = float(alpha)
-    train_frac = float(train_frac)
-    n_iters = int(n_iters)
-
-    if isinstance(project, str):
+    
+    if use_wandb:
         wandb.init(
-            project=project,
+            project=PROJECT_NAME,
             config={
                 'k1': k1,
                 'k2': k2,
                 'k3': k3,
                 'alpha': alpha,
                 'seed': seed,
+                'minibatch_size': minibatch_size,
+                'n_epochs': n_epochs,
             }
         )
+        wandb.define_metric('avg_lp', summary='min')
     
-    # Load data from input directory
-    # TODO Currently limit files loaded to debug OOM issue
-    fpath_list = sorted([f for f in Path(datadir).iterdir() if f.is_file()])
-    data = load_data(fpath_list[:1])
+    # ==========================================================================
 
     key = jr.PRNGKey(seed)
     key_mask, key_fit = jr.split(key)
 
-    batch_dims = _get_subshape(data['counts'], data['batch_axes'])
+    # Load data from input directory
+    fpath_list = sorted([f for f in Path(datadir).iterdir() if f.is_file()])
+    data = load_data(fpath_list)
 
-    # Mask out validation data
-    mask = make_random_mask(key_mask, batch_dims, train_frac)
+    # Create random mask to hold-out data for validation
+    batch_shape = _get_subshape(data['X'], data['batch_axes'])
+    mask = make_random_mask(key_mask, batch_shape, train_frac)
 
-    # Fit the model using the random seed provided
-    print('data shape', data['counts'].shape)
-    counts = jnp.asarray(data['counts'], dtype=jnp.float32)
+    # Cast integer counts to float32 dtype
+    X = jnp.asarray(data['X'], dtype=jnp.float32)
     del data
 
-    model, params, lps, pct_dev = \
-        fit_model(key_fit, counts, mask, k1, k2, k3, alpha, n_iters=n_iters)
-    # acc, confusion_matrix = evaluate_prediction(params["Psi"], y)
+    # Fit the model
+    params, lps = stochastic_fit_model(
+        key_fit, X, mask, k1, k2, k3, alpha, n_epochs=n_epochs,
+    )
 
-    if isinstance(project, str):
+    # Evaluate the model on heldout samples
+    test_ll, pct_dev = evaluate_fit(params, X, mask, k1, k2, k3, alpha)
+
+    # ==========================================================================
+    # Log metrics
+    if use_wandb:
         for lp in lps:
-            wandb.log({'lp': lp}, commit=False)
+            wandb.log({'avg_lp': lp / mask.sum()}, commit=False)
         wandb.run.summary["pct_dev"] = pct_dev
+        wandb.run.summary["avg_test_ll"] = test_ll / (~mask).sum()
         wandb.finish()
+
     return
 
 if __name__ == "__main__":
