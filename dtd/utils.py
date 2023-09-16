bf50accce74709a3b1413ac592854ad274d4353a
libby <elizabeth.zhang94@gmail.com>
2023-09-16 07:58:15 -0700

    changed default mem_frac, added more docs

--- dtd/utils.py
+++ dtd/utils.py
@@ -1,10 +1,22 @@
 import jax.numpy as jnp
 import jax.random as jr
 
-def calculate_minibatch_size(d1,d2,d3,k1,k2,k3,mem_gb,mem_frac=1):
+def calculate_minibatch_size(d1,d2,d3,k1,k2,k3,mem_gb,mem_frac=0.75):
     """Calculate minibatch size that maximizes available memory.
     
     Assumes 3D model with batch_dims (0,1).
+
+    If setting mem_frac > 0.75 (the default fraction of GPU that JAX allocates
+    on start up), make sure to adjust the environmental variable appropriately:
+        XLA_PYTHON_CLIENT_MEM_FRACTION=<mem_frac>
+
+    Note that the resulting minibatch size may not minimize the size of the last
+    incomplete batch. To then identify a better minibatch size, you could sweep
+    through minibatch sizes up to this returned value. For example:
+        import plotly.express as px
+        mods = [(m, (d1,d2) % m) for m in range(m_start,m)]
+        mod_m, mod_val = list(zip(*mods))
+        px.line(x=mod_m, y=mod_val)
     """
     m = (mem_gb*mem_frac) * (1024**3) / 4
     #m -= (d1*d2*d3)
