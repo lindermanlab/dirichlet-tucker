0a82886ff29fd3fbc794d3d4d468b733e8a2073f
Libby Zhang <ezhang94@github.com>
2023-11-18 06:09:09 -0800

    resolved file re-upload to wnb error

--- analysis/kf_lofo.py
+++ analysis/kf_lofo.py
@@ -1,4 +1,4 @@
-"""Principal factors analysis -- leave-one-factor-in (lofi) approach.
+"""Principal factors analysis -- leave-one-factor-out (lofo) approach.
 
 Identify the principle factors of analysis
 Evaluate held-out log likelihood to sort factors by importance.
@@ -12,46 +12,39 @@ e.g. avg_test_lps, avg_baseline_ll + etc, avg_lofo_test_ll_1 + etc, seed, run_id
 """
 
 from __future__ import annotations
+import os
 from pathlib import Path
 import wandb
+import time
 
+import numpy as onp
 import jax.numpy as jnp
 import jax.random as jr
 
 from dtd.model3d import DirichletTuckerDecomp
 from dtd.utils import get_wnb_project_df, download_wnb_params
 
-from killifish import load_data, evaluate_fit
+from killifish import load_data, evaluate_fit, make_random_mask
 
 import matplotlib.pyplot as plt
 from kf_viz import draw_syllable_factors, draw_circadian_bases
 
-
-# --------------------------------------------------------------
-
-# Hardcode run information in for now
-run_id = 'ig6dh2fo'
-run_seed = 1698740237
-
-# eternel-sweep 28
-# run_id = '46grddu7'
-# run_seed = 1698499854
-
-# --------------------------------------------------------------
 # --------------------------------------------------------------
 
 wnb_entity = 'eyz'
 wnb_project = 'kf-dtd-231022'
-OUT_DIR =  Path('/scratch/groups/swl1/killifish/p3_20230726-20230915/kf-dtd-231022')
+OUT_DIR =  Path('/scratch/groups/swl1/killifish/p3_20230726-20230915') / wnb_project
 DATA_DIR = Path('/scratch/groups/swl1/killifish/p3_20230726-20230915/q2-aligned_10min')
 
 # --------------------------------------------------------------
+# We can save time by loading data once!! However, make to ignore mask
+total_counts, X, _, _, _ = load_data(DATA_DIR, key=None)
 
 def main(run_id, seed, k1, k2, k3):
-    # Load data, in the same way that it was originally loaded
+    # Generate mask, as it was generated in the original script
     key = jr.PRNGKey(seed)
     key_mask, _ = jr.split(key)
-    total_counts, X, mask, _, _ = load_data(DATA_DIR, key=key_mask)
+    mask = make_random_mask(key_mask, X.shape[:-1], train_frac=0.8)
     
     # Load fitted params
     params = download_wnb_params(wnb_entity, wnb_project, run_id, OUT_DIR)
@@ -67,13 +60,16 @@ def main(run_id, seed, k1, k2, k3):
                                         = evaluate_fit(model, X, mask, params)
     
     # Sort parameters by their variance explained
+    print("Sorting parameters...", end='')
     params, lofo_test_lls = model.sort_params(X, mask, params)
- 
+    print("Done.")
+    
     # Save results locally
-    fpath_params = OUT_DIR/run_id/'params.npz'
-    n_train = mask.sum()
+    run_root_dir = OUT_DIR / run_id
+    
+    fpath_params = run_root_dir / 'params.npz'
     n_test = (~mask).sum()
-    jnp.savez_compressed(fpath_params,
+    onp.savez_compressed(fpath_params,
                          G=params[0],
                          F1=params[1],
                          F2=params[2],
@@ -92,27 +88,29 @@ def main(run_id, seed, k1, k2, k3):
     # Visualize parameters and save them
     fig_topics = plt.figure(figsize=(16, 4.5), dpi=96)
     draw_syllable_factors(params, ax=plt.gca())
-    fpath_topics = OUT_DIR/run_id/'behavioral-topics.png'
+    fpath_topics = run_root_dir/'behavioral-topics.png'
     plt.savefig(fpath_topics, bbox_inches='tight')
     plt.close()
 
     fig_bases = draw_circadian_bases(params)
-    fpath_bases = OUT_DIR/run_id/'circadian-bases.png'
+    fpath_bases = run_root_dir/'circadian-bases.png'
     plt.savefig(fpath_bases, bbox_inches='tight')
     plt.close()
 
-    # Now, upload everything back up to WandB
-    run = wandb.api.run(f"{wnb_entity}/{wnb_project}/{run_id}")
-
-    run.upload_file(str(fpath_params))
-    run.upload_file(str(fpath_topics))
-    run.upload_file(str(fpath_bases))
+    # Sync these files with WnB run
+    run = wandb.Api().run(f"{wnb_entity}/{wnb_project}/{run_id}")
+    run.upload_file(str(fpath_params), root=str(run_root_dir))
+    run.upload_file(str(fpath_topics), root=str(run_root_dir))
+    run.upload_file(str(fpath_bases), root=str(run_root_dir))
+    wandb.finish()
     return
-  
+
 # Retrieve "id", "name", "seed"
 config_keys, summary_keys = ['seed', 'k1', 'k2', 'k3'], []
 df = get_wnb_project_df(wnb_entity, wnb_project, config_keys, summary_keys)
 
 for i_df, run_info in df.iterrows():
+    start_time = time.time()
     print(run_info)
     main(run_info['id'], run_info['seed'], run_info['k1'], run_info['k2'], run_info['k3'])
+    print("run time:", time.time() - start_time)
